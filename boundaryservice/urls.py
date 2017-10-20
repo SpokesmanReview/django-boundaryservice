f31159dd10c7bbdfe73e198c47e9c72b22ef607a
Sean Stoops <brutimus@gmail.com>
2017-10-20 13:59:31 -0700

    Update urls.py
    Updating urls for Django 1.8 compat

--- boundaryservice/urls.py
+++ boundaryservice/urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls.defaults import patterns, include 
+from django.conf.urls import include, url
 from tastypie.api import Api
 
 from boundaryservice.resources import BoundarySetResource, BoundaryResource
-from boundaryservice.views import external_id_redirects
+from boundaryservice import views as bs_views
 
 v1_api = Api(api_name='1.0')
 v1_api.register(BoundarySetResource())
 v1_api.register(BoundaryResource())
 
-urlpatterns = patterns('',
-    (r'^(?P<api_name>1.0)/(?P<resource_name>boundary-set)/(?P<slug>[\w\d_.-]+)/(?P<external_id>[\w\d_.-]+)$', external_id_redirects),
-    (r'', include(v1_api.urls)),
-)
+urlpatterns = [
+    url(r'^(?P<api_name>1.0)/(?P<resource_name>boundary-set)/(?P<slug>[\w\d_.-]+)/(?P<external_id>[\w\d_.-]+)$', bs_views.external_id_redirects),
+    url(r'', include(v1_api.urls))
+]
